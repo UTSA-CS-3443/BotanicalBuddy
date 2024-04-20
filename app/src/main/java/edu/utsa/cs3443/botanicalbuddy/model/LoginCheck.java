7db25faeec2d8edce260cca77f5ec4641a1deef0
Winden79 <devindunn79@gmail.com>
2024-04-19 19:26:07 -0500

    added rough draft of registration, errors out since apparently accounts.csv is compressed or something and every work around i find doesn't seem to work.

--- app/src/main/java/edu/utsa/cs3443/botanicalbuddy/model/LoginCheck.java
+++ app/src/main/java/edu/utsa/cs3443/botanicalbuddy/model/LoginCheck.java
@@ -1,19 +1,49 @@
 package edu.utsa.cs3443.botanicalbuddy.model;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import android.content.res.AssetManager;
 
+import androidx.annotation.NonNull;
+
 import edu.utsa.cs3443.botanicalbuddy.LoginActivity;
+import edu.utsa.cs3443.botanicalbuddy.RegistrationActivity;
 
 
 public class LoginCheck {
     private String username;
     private String password;
+    private String hint;
+
+    public static void addAccount(String username, String password, String hint, RegistrationActivity activity) throws IOException {
+        AssetManager manager = activity.getAssets();
+        File tempFile = File.createTempFile("tempAccounts", null, activity.getCacheDir());
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+        writer.write(username + "," + password + "," + hint);
+        writer.newLine();
+        writer.flush();
+        writer.close();
+        InputStream in = new FileInputStream(tempFile);
+        OutputStream out = manager.openFd("accounts.csv").createOutputStream();
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = in.read(buffer)) > 0) {
+            out.write(buffer, 0, length);
+        }
+
+        in.close();
+        out.close();
+    }
 
     public static Boolean validLogin(String username, String password, LoginActivity activity) throws IOException {
         AssetManager manager = activity.getAssets();
